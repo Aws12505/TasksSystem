e4e50a8a950defa05de71a9f366fd21c4ed9ecf6
Aws12505 <awsaljaramani12505@gmail.com>
2025-11-14 08:39:18 -0500

    edits for pizza

--- Tasks_Front/dist/assets/useRole-BTFXC9dD.js
+++
@@ -1,1 +0,0 @@
-import{R as l}from"./index-XLlDukUQ.js";import{u}from"./rolesStore-R7ezOoA4.js";const p=e=>{const{currentRole:i,availablePermissions:a,isLoading:n,error:c,fetchRole:s,fetchAvailablePermissions:o,clearCurrentRole:t}=u(),r=typeof e=="string"?parseInt(e):e;return l.useEffect(()=>(r&&(s(r),o()),()=>{t()}),[r,s,o,t]),{role:i,availablePermissions:a,isLoading:n,error:c}};export{p as u};

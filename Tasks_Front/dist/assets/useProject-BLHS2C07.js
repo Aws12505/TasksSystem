10a48517875261e4229d319467624d10c0192a4c
Aws12505 <awsaljaramani12505@gmail.com>
2025-11-17 09:03:57 -0500

    pizza

---
+++ Tasks_Front/dist/assets/useProject-BLHS2C07.js
@@ -0,0 +1,1 @@
+import{an as f,R as j}from"./index-Cj_ZlKJu.js";const p=e=>{const{currentProject:c,projectSections:a,availableUsers:n,isLoading:i,error:u,fetchProject:t,fetchAvailableUsers:s,clearCurrentProject:o}=f(),r=typeof e=="string"?parseInt(e):e;return j.useEffect(()=>(r&&(t(r),s()),()=>{o()}),[r,t,s,o]),{project:c,sections:a,availableUsers:n,isLoading:i,error:u}};export{p as u};
